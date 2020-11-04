bd42f23e64cbe1750358c35d0fae4ad84af3ba35
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-11-04 17:19:54 +0200

    refactored user to usr

--- src/main/java/fi/lehtinen/Scoretracker/web/UserDetailServiceImpl.java
+++ src/main/java/fi/lehtinen/Scoretracker/web/UserDetailServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import fi.lehtinen.Scoretracker.domain.User;
+import fi.lehtinen.Scoretracker.domain.Usr;
 import fi.lehtinen.Scoretracker.domain.UserRepository;
 
 @Service
@@ -22,7 +22,7 @@ public class UserDetailServiceImpl implements UserDetailsService  {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {   
-    	User curruser = repository.findByUsername(username);
+    	Usr curruser = repository.findByUsername(username);
         UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
         		AuthorityUtils.createAuthorityList(curruser.getRole()));
         return user;
