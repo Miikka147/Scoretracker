8f7dcf58b7a30fed4cff36a2a063addeafebc10d
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-12-17 14:22:22 +0200

    added application properties

--- src/main/java/fi/lehtinen/Scoretracker/domain/CourseRepository.java
+++ src/main/java/fi/lehtinen/Scoretracker/domain/CourseRepository.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 	public interface CourseRepository extends CrudRepository<Course, Long> {
 
 	    Optional<Course> findById(Long id);
 	    List<Course> findByName(String name);
 		Object getOne(Long courseId);
 	    
+		Course save(Course course);
 	}
 
