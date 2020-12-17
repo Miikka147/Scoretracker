9636247fbe72fa8ffab0ec97ff0cdf27783418f4
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-12-17 14:38:33 +0200

    changes

--- src/main/java/fi/lehtinen/Scoretracker/ScoretrackerApplication.java
+++ src/main/java/fi/lehtinen/Scoretracker/ScoretrackerApplication.java
@@ -1,8 +1,17 @@
 package fi.lehtinen.Scoretracker;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.lehtinen.Scoretracker.domain.CourseRepository;
+import fi.lehtinen.Scoretracker.domain.GameRepository;
+import fi.lehtinen.Scoretracker.domain.HoleRepository;
+import fi.lehtinen.Scoretracker.domain.UsrRepository;
+
+
 
 
 
@@ -13,5 +22,11 @@ public class ScoretrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScoretrackerApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner Ticketguru(CourseRepository courserepository,GameRepository gamerepository,HoleRepository holerepository,UsrRepository usrrepository) {
+		return (args) -> {
+	
+		};
+	}
+}
 
-} 
