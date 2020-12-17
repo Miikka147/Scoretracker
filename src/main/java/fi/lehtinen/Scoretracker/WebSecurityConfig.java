9125b79aa2dbebec502f8f399b17a7a98c0fced8
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-12-17 15:55:33 +0200

    trying fix

--- src/main/java/fi/lehtinen/Scoretracker/WebSecurityConfig.java
+++ src/main/java/fi/lehtinen/Scoretracker/WebSecurityConfig.java
@@ -24,14 +24,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
         
-        .csrf().disable()
         .authorizeRequests().antMatchers("/css/**").permitAll() 
         .and()
         .authorizeRequests()
           .anyRequest().authenticated()
           .and()
-          .authorizeRequests()
-  		.antMatchers(HttpMethod.OPTIONS,"/**").permitAll().and()
       .formLogin()
           .defaultSuccessUrl("/courselist")
           .permitAll()
